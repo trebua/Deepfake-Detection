f75cd1ff2fc492c1e18b94e85c1f8adc0afff9b2
Aubert <sindreaubert@gmail.com>
2020-04-23 08:58:27 +0200

    Add balancing to preprocess

--- preprocess.py
+++ preprocess.py
@@ -35,17 +35,18 @@ def create_processed_dirs():
     return processed_path
 
 
-def preprocess_videos(dimensions=(100,100), sample=10, count=False, face_threshold=0.90, face_attempts=5, save=True):
+def preprocess_videos(dimensions=(100,100), base_sample=5, count=False, face_threshold=0.90, base_attempts=50, ratio=9):
     '''
     Runs through all the videos downloaded and stores the faces found in each video.
 
     Args:
         dimensions: What width and height the frame will be stored with
-        sample: How many frames will be saved from each video
+        base_sample: How many frames will be saved from the label with most videos
         count: How many videos will be processed from real and fake
         face_threshold: How strict the cvlib face recognition should be. Higher is stricter and yields clearer faces.
         face_attempts: How many frames will be tested for faces before giving up on a video
-        save: If the resulting training data-arrays should be saved 
+        base_attempts: How many attempts for finding faces for a label with the most videos
+        ratio: most videos/least videos - to balance amount of labelled faces
 
     Returns:
         None
@@ -53,10 +54,16 @@ def preprocess_videos(dimensions=(100,100), sample=10, count=False, face_thresho
     X, y = [], []
     processed_path = create_processed_dirs()
     for label, label_path in datasets.items():  # Loop through labels (real, fake) with their corresponding dataset paths
-        processed = 0
         path = current_dir + data_dir + label_path
         videos = os.listdir(path)
         avg_time = 0
+        processed = 0
+        if label == 'real':
+            sample = base_sample*ratio
+            max_attempts = base_attempts*5
+        else:
+            sample = base_sample
+            max_attempts = base_attempts
         for c, video_name in enumerate(videos):
             start_time = time()
             video_path = path + video_name
@@ -69,26 +76,25 @@ def preprocess_videos(dimensions=(100,100), sample=10, count=False, face_thresho
             random.shuffle(frames)  # Shuffles the frames in order to pick next frame randomly
             frames_saved = 0
             attempts = 0
-            while frames_saved < sample and attempts < face_attempts:
-                frame_index = frames[frames_saved]
-                video.set(1,frame_index)
-                name = f'{image_path}-{frames_saved+1}-'
-                _, frame = video.read() 
-                faces, confidences = cv.detect_face(frame, threshold=face_threshold)
-                if not len(faces):
-                    attempts +=1
-                else:
-                    frames_saved += 1
-                for j, (x0,y0,x1,y1) in enumerate(faces):
-                    face = frame[y0:y1, x0:x1]
-                    if len(face) > 0 and len(face[0] > 0):
-                        face = cv2.resize(face, dimensions, interpolation = cv2.INTER_AREA)
-                        filename = f'{name}face{j+1}.jpg'
-                        #cv2.imwrite(filename, face) #uncomment to save the processed faces (debugging)
-                        face = face.astype(np.float32)
-                        face /= 255
-                        X.append(face)
-                        y.append(int(label =='real'))
+            while frames_saved < sample and attempts < max_attempts and attempts < frame_count:
+                try:
+                    saved = False
+                    video.set(1,frames[attempts])
+                    attempts += 1
+                    _, frame = video.read() 
+                    faces, confidences = cv.detect_face(frame, threshold=face_threshold)
+                    for j, (x0,y0,x1,y1) in enumerate(faces):
+                        face = frame[y0:y1, x0:x1]
+                        if len(face) > 0 and len(face[0] > 0):
+                            face = cv2.resize(face, dimensions, interpolation = cv2.INTER_AREA)
+                            face = face.astype(np.float32)
+                            face /= 255
+                            X.append(face)
+                            y.append(int(label =='real'))
+                            saved = True
+                    frames_saved += int(saved)
+                except:
+                    break
             video.release() 
             cv2.destroyAllWindows()
 
@@ -101,9 +107,8 @@ def preprocess_videos(dimensions=(100,100), sample=10, count=False, face_thresho
             if count and processed >= count:
                 break
     X, y = np.array(X), np.array(y)
-    if save:
-        np.save(f'{processed_path}X', X)
-        np.save(f'{processed_path}y', y)
+    np.save(f'{processed_path}X', X)
+    np.save(f'{processed_path}y', y)
     return X, y
     
 preprocess_videos()
